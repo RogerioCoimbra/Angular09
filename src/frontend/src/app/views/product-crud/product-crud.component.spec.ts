e71bc9ec3d4d5278f4b3e99d85a6fa2e23691951
Rogerio Coimbra <rogeriocoimbra@gmail.com>
2020-05-17 21:09:25 -0300

    Add products and routes

--- src/frontend/src/app/views/product-crud/product-crud.component.spec.ts
+++ src/frontend/src/app/views/product-crud/product-crud.component.spec.ts
@@ -1,19 +1,19 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { HomeComponent } from './home.component';
+import { ProductCrudComponent } from './product-crud.component';
 
-describe('HomeComponent', () => {
-  let component: HomeComponent;
-  let fixture: ComponentFixture<HomeComponent>;
+describe('ProductCrudComponent', () => {
+  let component: ProductCrudComponent;
+  let fixture: ComponentFixture<ProductCrudComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [HomeComponent],
+      declarations: [ProductCrudComponent],
     }).compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(HomeComponent);
+    fixture = TestBed.createComponent(ProductCrudComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
